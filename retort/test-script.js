c9a1df9a438ff4e898fbaa538d776a613eaf45de
danielhuggins <diegohuggins@gmail.com>
2024-02-17 11:03:10 +0000

    Tweaked the test script to be more accessible.

--- retort/test-script.js
+++ retort/test-script.js
@@ -1,9 +1,7 @@
 // This magic line allows you to use this file as both a script and a module.
 module.exports = require("retort-js").script(async $ => {
 
-    $.system`You are 'Retorter', an AI that responds in a quick & witty manner.`
-
-    await $.user();
+    $.system`Write a haiku about LLMs (Large Language Models).`
 
     await $.assistant();
 
