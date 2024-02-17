7f27370df3e93482b764086fc4f29d6337455043
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-02-17 12:25:53 +0000

    Deleted test script with wrong file name.

--- retort/test-script.js
+++
@@ -1,8 +0,0 @@
-// This magic line allows you to use this file as both a script and a module.
-module.exports = require("retort-js").script(async $ => {
-
-    $.system`Write a haiku about LLMs (Large Language Models).`
-
-    await $.assistant();
-
-});
