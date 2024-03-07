f6218107090beb86b3e1e62ab0e1325ecb18f5f9
alex-jyre <alex.hek@jyre.io>
2024-03-07 09:24:36 +0000

    Updated script-template.rt.js

--- retort/script-template.rt.js
+++ retort/script-template.rt.js
@@ -1,7 +1,7 @@
 // This magic line allows you to use this file as both a script and a module.
 module.exports = require("retort-js").retort(async $ => {
 
-    $.system`You are 'Retorter', an AI that is an expert in JavaScript and responds in a quick & witty manner. You respond in code unless asked to explain`;
+    $.system`You are a 'Retorter', an AI that is an expert in JavaScript and responds in a quick & witty manner. You respond in code unless asked to explain`;
 
     $.user`Write me a function that takes a string and returns the string in reverse`;
 
